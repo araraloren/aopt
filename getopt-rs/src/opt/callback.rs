e1b73d3db581939b431ec9c0f6d7d16b89c4081d
araraloren <araraloren@gmail.com>
2021-06-30 21:07:49 +0800

    add index/indexmut callback

--- getopt-rs/src/opt/callback.rs
+++ getopt-rs/src/opt/callback.rs
@@ -3,9 +3,15 @@ use std::fmt::Debug;
 
 use crate::err::Result;
 use crate::opt::Opt;
+use crate::set::Set;
+use crate::uid::Uid;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub enum CallbackType {
+    Index, 
+
+    IndexMut,
+
     Value,
 
     ValueMut,
@@ -24,6 +30,20 @@ impl Default for CallbackType {
 }
 
 impl CallbackType {
+    pub fn is_index(&self) -> bool {
+        match self {
+            Self::Index => true,
+            _ => false,
+        }
+    }
+
+    pub fn is_index_mut(&self) -> bool {
+        match self {
+            Self::IndexMut => true,
+            _ => false,
+        }
+    }
+
     pub fn is_value(&self) -> bool {
         match self {
             Self::Value => true,
@@ -58,4 +78,58 @@ impl CallbackType {
             _ => false,
         }
     }
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait IndexCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &dyn Set) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &dyn Set) -> Result<bool>;
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait IndexMutCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &mut dyn Set) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &mut dyn Set) -> Result<bool>;
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait ValueCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &dyn Set, arg: &String) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &dyn Set, arg: &String) -> Result<bool>;
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait ValueMutCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &mut dyn Set, arg: &String) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &mut dyn Set, arg: &String) -> Result<bool>;
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait MainCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &dyn Set, args: &Vec<String>) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &dyn Set, args: &Vec<String>) -> Result<bool>;
+}
+
+#[async_trait::async_trait(?Send)]
+pub trait MainMutCallback: Debug {
+    #[cfg(not(feature="async"))]
+    fn call(&mut self, uid: Uid, set: &mut dyn Set, args: &Vec<String>) -> Result<bool>;
+
+    #[cfg(feature="async")]
+    async fn call(&mut self, uid: Uid, set: &mut dyn Set, args: &Vec<String>) -> Result<bool>;
 }
