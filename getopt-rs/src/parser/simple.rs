83de3ee262f80e51bd99244ecd8dc531edce43f2
araraloren <araraloren@gmail.com>
2021-07-18 22:34:04 +0800

    refactor test

--- getopt-rs/src/parser/simple.rs
+++ getopt-rs/src/parser/simple.rs
@@ -321,8 +321,10 @@ where
 #[cfg(test)]
 mod test {
     use crate::{prelude::*, set::Commit};
+    use std::marker::PhantomData;
 
-    struct ValueChecker {
+    #[derive(Debug)]
+    struct CheckingData {
         type_name: &'static str,
 
         deactivate_style: bool,
@@ -342,6 +344,20 @@ mod test {
         index: Option<OptIndex>,
     }
 
+    struct TestingCase<S: Set, P: Parser<S>> {
+        opt_str: &'static str,
+
+        after_parse_value: OptValue,
+
+        commit_tweak: Option<Box<dyn FnMut(&mut Commit)>>,
+
+        callback_tweak: Option<Box<dyn FnMut(&mut P, Uid)>>,
+
+        checking: CheckingData,
+
+        marker: PhantomData<S>,
+    }
+
     #[test]
     fn testing_simple_parser() {
         let mut set = SimpleSet::new();
@@ -349,93 +365,59 @@ mod test {
 
         set.add_prefix(String::from("+"));
 
-        assert!(testing_add_opts(&mut set, &mut parser).is_ok());
+        // assert!(testing_add_opts(&mut set, &mut parser).is_ok());
 
         let ret = parser.parse(set, &mut ["+intalias", "42"].iter().map(|v| v.to_string()));
 
         assert!(ret.is_ok());
-        assert!(ret.unwrap().is_some());
+
+        let retvalue = ret.unwrap();
+
+        assert!(retvalue.is_some());
+
+        if let Some(mut retvalue) = retvalue {
+            assert!(checking_value(&mut retvalue.set, "i", OptValue::Int(42)).is_ok());
+        }
     }
 
-    fn testing_add_opts<S: Set, P: Parser<S>>(set: &mut S, parser: &mut P) -> Result<()> {
-        testing_add_opt(
-            set,
-            parser,
-            "-i=i",
-            None,
-            ValueChecker {
-                type_name: "i",
-                deactivate_style: false,
-                value: OptValue::from(42i64),
-                default_value: OptValue::Null,
-                name: "i",
-                prefix: "-",
-                alias: vec![("+", "intalias")],
-                optional: true,
-                index: None,
-            },
-        )?;
-        testing_add_opt(
-            set,
-            parser,
-            "--longint=i",
-            None,
-            ValueChecker {
-                type_name: "i",
-                deactivate_style: false,
-                value: OptValue::from(24i64),
-                default_value: OptValue::Null,
-                name: "longint",
-                prefix: "--",
-                alias: vec![],
-                optional: true,
-                index: None,
-            },
-        )?;
+    fn checking_value<S: Set>(set: &mut S, opt: &str, value: OptValue) -> Result<()> {
+        let filter = set.filter(opt)?;
+        let opt = filter.find();
+
+        if let Some(opt) = opt {
+            assert_eq!(*opt.as_ref().get_value(), value);
+        }
         Ok(())
     }
 
-    fn testing_add_opt<S: Set, P: Parser<S>>(
+    fn testing_add_opts<S: Set, P: Parser<S>>(
         set: &mut S,
         parser: &mut P,
-        opt: &str,
-        tweaker: Option<Box<dyn FnMut(&mut Commit)>>,
-        checker: ValueChecker,
+        ts: &mut TestingCase<S, P>,
     ) -> Result<()> {
-        let mut commit = set.add_opt(opt)?;
-
-        if let Some(mut tweaker) = tweaker {
-            tweaker.as_mut()(&mut commit);
+        let mut commit = set.add_opt(ts.opt_str)?;
+        if let Some(tweak) = ts.commit_tweak.as_mut() {
+            tweak.as_mut()(&mut commit);
         }
-
         let id = commit.commit()?;
-
-        parser.add_callback(
-            id,
-            simple_opt_cb!(move |inner_id, set, value| {
-                assert_eq!(id, inner_id);
-                assert_eq!(value, checker.value);
-
-                let opt = set.get_opt(inner_id);
-
-                assert!(opt.is_some());
-
-                let opt = opt.unwrap();
-
-                assert_eq!(opt.get_name(), checker.name);
-                assert_eq!(opt.is_need_invoke(), true);
-                assert_eq!(opt.get_optional(), checker.optional);
-                assert_eq!(checker.default_value, *opt.get_default_value());
-                assert_eq!(opt.get_type_name(), checker.type_name);
-                assert_eq!(checker.deactivate_style, opt.is_deactivate_style());
-                assert_eq!(checker.prefix, opt.get_prefix());
-                assert_eq!(opt.get_index(), checker.index.as_ref());
-                for (prefix, name) in &checker.alias {
-                    assert!(opt.match_alias(prefix, name));
-                }
-                Ok(Some(value))
-            }),
-        );
+        if let Some(tweak) = ts.callback_tweak.as_mut() {
+            tweak.as_mut()(parser, id);
+        }
         Ok(())
+    }
+
+    fn checking_data(opt: &dyn Opt, checking_data: &CheckingData, value: &OptValue) {
+        assert_eq!(opt.get_name(), checking_data.name);
+        assert_eq!(opt.is_need_invoke(), true);
+        assert_eq!(opt.get_optional(), checking_data.optional);
+        assert!(checking_data.default_value.eq(opt.get_default_value()));
+        assert_eq!(opt.get_type_name(), checking_data.type_name);
+        assert_eq!(checking_data.deactivate_style, opt.is_deactivate_style());
+        assert_eq!(checking_data.prefix, opt.get_prefix());
+        assert_eq!(opt.get_index(), checking_data.index.as_ref());
+        for (prefix, name) in &checking_data.alias {
+            assert!(opt.match_alias(prefix, name));
+        }
+        assert!(checking_data.value.eq(value));
     }
 }
