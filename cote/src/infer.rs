79944ff32a7a9d17223440dde08958123e1f185d
araraloren <blackcatoverwall@gmail.com>
2023-01-01 21:17:12 +0800

    update infer

--- cote/src/infer.rs
+++ cote/src/infer.rs
@@ -1,23 +1,39 @@
 use aopt::prelude::{Assoc, Action};
 
+enum InferConvert {
+    AsRef,
+
+    AsMut,
+
+    Deref,
+
+    DerefMut,
+
+    Clone,
+
+    Copy,
+
+    AsCopy,
+
+    Null,
+}
+
 pub trait Infer {
     type Val;
-
-    type Ref<'a>;
 
     fn ty() -> &'static str;
 
     fn assoc() -> Assoc;
 
     fn action() -> Action;
+
+    fn converter() -> InferConvert;
 }
 
 macro_rules! infer_type_def {
-    ($ty:ident, $val:ident, &$a:lifetime $ref:ident, $name:literal, $assoc:expr, $action:expr) => {
+    ($ty:ident, $val:ident, $name:literal, $assoc:expr, $action:expr, $converter:expr) => {
             impl Infer for $ty {
                 type Val = $val;
-
-                type Ref<$a> = &$a $ref;
 
                 fn ty() -> &'static str {
                     $name
@@ -29,17 +45,19 @@ macro_rules! infer_type_def {
             
                 fn action() -> Action {
                     $action
+                }
+
+                fn converter() -> InferConvert {
+                    $converter
                 }
             }
     };
 }
 
 macro_rules! infer_reftype_def {
-    (&$b:lifetime $ty:ident, $val:ident, &$a:lifetime $ref:ident, $name:literal, $assoc:expr, $action:expr) => {
-            impl<$b> Infer for &$b $ty {
+    (&$a:lifetime $ty:ident, $val:ident, $name:literal, $assoc:expr, $action:expr, $converter:expr) => {
+            impl<$a> Infer for &$a $ty {
                 type Val = $val;
-
-                type Ref<$a> = &$a $ref;
 
                 fn ty() -> &'static str {
                     $name
@@ -52,39 +70,49 @@ macro_rules! infer_reftype_def {
                 fn action() -> Action {
                     $action
                 }
+
+                fn converter() -> InferConvert {
+                    $converter
+                }
             }
     };
 }
 
-infer_type_def!(bool, bool, &'a bool, "b", Assoc::Bool, Action::Set);
-infer_reftype_def!(&'b bool, bool, &'a bool, "b", Assoc::Bool, Action::Set);
+infer_type_def!(bool, bool, "b", Assoc::Bool, Action::Set, InferConvert::Copy);
+infer_reftype_def!(&'b bool, bool, "b", Assoc::Bool, Action::Set, InferConvert::Null);
 
-infer_type_def!(String, String, &'a String, "s", Assoc::Str, Action::App);
-infer_reftype_def!(&'b String, String, &'a String, "s", Assoc::Str, Action::App);
-infer_reftype_def!(&'b str, String, &'a String, "s", Assoc::Str, Action::App);
+infer_type_def!(String, String, "s", Assoc::Str, Action::App, InferConvert::Clone);
+infer_reftype_def!(&'b String, String, "s", Assoc::Str, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b str, String, "s", Assoc::Str, Action::App, InferConvert::AsRef);
 
-infer_type_def!(i64, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_type_def!(i32, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_type_def!(i16, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_type_def!( i8, i64, &'a i64, "i", Assoc::Int, Action::App);
+infer_type_def!(i64, i64, "i", Assoc::Int, Action::App, InferConvert::Copy);
+infer_type_def!(i32, i64, "i", Assoc::Int, Action::App, InferConvert::AsCopy);
+infer_type_def!(i16, i64, "i", Assoc::Int, Action::App, InferConvert::AsCopy);
+infer_type_def!( i8, i64, "i", Assoc::Int, Action::App, InferConvert::AsCopy);
 
-infer_type_def!(u64, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_type_def!(u32, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_type_def!(u16, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_type_def!( u8, u64, &'a u64, "u", Assoc::Uint, Action::App);
+infer_type_def!(u64, u64, "u", Assoc::Uint, Action::App, InferConvert::Copy);
+infer_type_def!(u32, u64, "u", Assoc::Uint, Action::App, InferConvert::AsCopy);
+infer_type_def!(u16, u64, "u", Assoc::Uint, Action::App, InferConvert::AsCopy);
+infer_type_def!( u8, u64, "u", Assoc::Uint, Action::App, InferConvert::AsCopy);
 
-infer_type_def!(f64, i64, &'a f64, "f", Assoc::Flt, Action::App);
-infer_type_def!(f32, i64, &'a f64, "f", Assoc::Flt, Action::App);
+infer_type_def!(f64, i64, "f", Assoc::Flt, Action::App, InferConvert::Copy);
+infer_type_def!(f32, i64, "f", Assoc::Flt, Action::App, InferConvert::AsCopy);
 
-infer_reftype_def!(&'b i64, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_reftype_def!(&'b i32, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_reftype_def!(&'b i16, i64, &'a i64, "i", Assoc::Int, Action::App);
-infer_reftype_def!(&'b  i8, i64, &'a i64, "i", Assoc::Int, Action::App);
+infer_type_def!(isize, isize, "i", Assoc::Int, Action::App, InferConvert::AsCopy);
+infer_type_def!(usize, usize, "u", Assoc::Uint, Action::App, InferConvert::AsCopy);
 
-infer_reftype_def!(&'b u64, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_reftype_def!(&'b u32, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_reftype_def!(&'b u16, u64, &'a u64, "u", Assoc::Uint, Action::App);
-infer_reftype_def!(&'b  u8, u64, &'a u64, "u", Assoc::Uint, Action::App);
+infer_reftype_def!(&'b i64, i64, "i", Assoc::Int, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b i32, i64, "i", Assoc::Int, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b i16, i64, "i", Assoc::Int, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b  i8, i64, "i", Assoc::Int, Action::App, InferConvert::Null);
 
-infer_reftype_def!(&'b f64, i64, &'a f64, "f", Assoc::Flt, Action::App);
-infer_reftype_def!(&'b f32, i64, &'a f64, "f", Assoc::Flt, Action::App);
+infer_reftype_def!(&'b u64, u64, "u", Assoc::Uint, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b u32, u64, "u", Assoc::Uint, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b u16, u64, "u", Assoc::Uint, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b  u8, u64, "u", Assoc::Uint, Action::App, InferConvert::Null);
+
+infer_reftype_def!(&'b f64, i64, "f", Assoc::Flt, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b f32, i64, "f", Assoc::Flt, Action::App, InferConvert::Null);
+
+infer_reftype_def!(&'b isize, isize, "i", Assoc::Int, Action::App, InferConvert::Null);
+infer_reftype_def!(&'b usize, usize, "u", Assoc::Uint, Action::App, InferConvert::Null);
